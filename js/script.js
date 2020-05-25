c24c2b0833dd9f896ed74245afafbe125f84960c
Nicola Valente <nicolavalente88@gmail.com>
2020-05-25 17:52:14 +0200

    Esercizio Uno finito

--- js/script.js
+++ js/script.js
@@ -3,27 +3,33 @@
 var iscritti = ["Marco@gmail.com", "Luca@gmail.com", "Paola@gmail.com", "Ernesto@gmail.com", "Francesca@gmail.com"];
 
 //Registrazione mail utente
-var mail = prompt("Inserisci il tuo indirizzo Email");
+var mail = prompt("✉ Inserisci il tuo indirizzo Email");
 iscritti.push(mail);
 console.log("mail di registrazione", mail);
 console.log("elenco iscritti", iscritti);
 
 //login utente
-var login = prompt("Fai login");
+var login = prompt("⏩ Fai login");
 console.log("mail di login:", login);
+
+//Variabile di appogg
+var validazioneLogin = false;
+
+//messaggio di eventuale errore a schermo
+var messaggio = document.getElementById("messaggio")
 
 //Controllo se la mail inserita é tra gli indirizzi disponibili
 for (var i = 0; i < iscritti.length; i++ ) {
   console.log("indice:", i);
   console.log("indirizzi riconosciuti:", iscritti[i]);
-
-  // pongo come istruzione condizionale che la mail inserita sia contenuta nel mio Array
   if (iscritti[i] === login) {
-    console.log("indirizzo esatto:", login);
-    alert("Indirizzo esatto!");
-  }else {
-    console.log("indirizzo sbagliato:", login);
-    var errore = document.getElementById('errore')
-    errore.innerHTML = "errore"
+    validazioneLogin = true;
   }
+}
+
+//validazione Login
+if (validazioneLogin) {
+  messaggio.innerHTML = "Ciao 👌 " + login;
+}else {
+  messaggio.innerHTML = "Errore " + login + " ⚠️ utente non riconosciuto";
 }
